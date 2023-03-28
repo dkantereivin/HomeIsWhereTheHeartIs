2011d8bc07beb864943c9337da23a58d5b00d43b
David Kanter Eivin <kanter.dev@gmail.com>
2023-03-28 02:20:18 -0400

    finished gameplay and cutscenes, some polish needed

--- characters/player_vars.gd
+++ characters/player_vars.gd
@@ -1,3 +1,14 @@
 extends Node
 
 var quests = []
+var inventory = []
+var completed = []
+
+func remove_sign(name: String):
+	var group = get_node("/root/ParkLevel/SignsGroup")
+	var node = group.get_node(name)
+	group.remove_child(node)
+	
+func sign_exists(name: String):
+	var group = get_node("/root/ParkLevel/SignsGroup")
+	return group.has_node(name)
--- levels/park_level.gd
+++ levels/park_level.gd
@@ -1,16 +1,39 @@
 extends Node2D
 
 @onready var player = get_node("Player")
+@onready var tools = get_node("Tools")
+@onready var hotdogs = get_node("HotdogStand")
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("interact"):
 		interact()
+	if PlayerVars.completed.size() == 2 and not $CutscenePlayer.is_playing():
+		$CutscenePlayer.play("RESET")
 
 func interact():
 	for houseless in get_node("HouselessGroup").get_children():
 		if player.get_position().distance_to(houseless.get_position()) < 50:
 			var resource_path = "res://sprites/houseless/dialogues/%s.dialogue"
-			var dialogue = load(resource_path % houseless.name)
-			await DialogueManager.show_example_dialogue_balloon(dialogue)
+			var dialogue = load(resource_path % (houseless.name + "Quest" if houseless.name in PlayerVars.quests else houseless.name))
+			DialogueManager.show_example_dialogue_balloon(dialogue)
 			return
 			
+	for sign in get_node("SignsGroup").get_children():
+		var dialogue = load("res://sprites/%s.dialogue" % sign.name)
+		if player.get_position().distance_to(sign.get_position()) < 25:
+			DialogueManager.show_example_dialogue_balloon(dialogue)
+			
+	if player.get_position().distance_to(tools.get_position()) < 25:
+		PlayerVars.inventory += ["Tools"]
+		remove_child(tools)
+		
+		var dialogue = load("res://sprites/Tool.dialogue")
+		DialogueManager.show_example_dialogue_balloon(dialogue)
+	
+	if player.get_position().distance_to(hotdogs.get_position()) < 40:
+		var dialogue = load("res://sprites/HotdogStand.dialogue")
+		DialogueManager.show_example_dialogue_balloon(dialogue)
+
+
+func game_end(anim_name):
+	get_tree().quit()
